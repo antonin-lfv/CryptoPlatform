3d178274dc2c907d7bccfc05d29d92429ab88a24
Antonin <antoninlefevre45@icloud.com>
2023-12-18 23:20:58 +0100

    faq, wallet, settings, nft and crypto pages

--- general/general.py
+++ general/general.py
@@ -16,3 +16,33 @@ def home():
 @login_required
 def profile():
     return render_template('general/profile.html', user=current_user)
+
+
+@BLP_general.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings():
+    return render_template('general/setting.html', user=current_user)
+
+
+@BLP_general.route('/faq', methods=['GET', 'POST'])
+@login_required
+def faq():
+    return render_template('general/faq.html', user=current_user)
+
+
+@BLP_general.route('/crypto_dashboard', methods=['GET', 'POST'])
+@login_required
+def crypto_dashboard():
+    return render_template('general/crypto_dashboard.html', user=current_user)
+
+
+@BLP_general.route('/nft_dashboard', methods=['GET', 'POST'])
+@login_required
+def nft_dashboard():
+    return render_template('general/nft_dashboard.html', user=current_user)
+
+
+@BLP_general.route('/wallet', methods=['GET', 'POST'])
+@login_required
+def wallet():
+    return render_template('general/wallets.html', user=current_user)
